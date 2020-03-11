851dcfac0c76f4fae106787421e08faf7e579df3
Alex Brombal <alex@brombal.com>
2020-03-10 19:39:47 -0500

    Updates

---
+++ precache-manifest.729baa9a85a53742904490194dba8e95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4555d27526f607d145d3b670686b1f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f910b606b9250068444",
+    "url": "/static/css/main.4924015b.chunk.css"
+  },
+  {
+    "revision": "8794da1e08d7ac4876f0",
+    "url": "/static/js/2.4f44aaf0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4f44aaf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f910b606b9250068444",
+    "url": "/static/js/main.697a6e7a.chunk.js"
+  },
+  {
+    "revision": "b1eb6d52be9b1d4617fc",
+    "url": "/static/js/runtime-main.81c1a2ac.js"
+  },
+  {
+    "revision": "edd96f6943d167516204b41516a78c4c",
+    "url": "/static/media/favicon.edd96f69.png"
+  }
+]);
